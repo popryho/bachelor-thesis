f729a663cfc1dbb111969513866f93fbbabc33bc
Leonid Popryho <18726515+popryho@users.noreply.github.com>
2021-05-14 21:06:26 +0300

    get_params, set_params added

--- Estimator.py
+++ Estimator.py
@@ -1,11 +1,10 @@
+import inspect
+from collections import defaultdict
+
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
 from tqdm import tqdm
 
-
-# TODO:
-#  add predict probabilities function
-#  add get params function
 
 # from sklearn.utils import check_X_y
 # from sklearn.utils.multiclass import check_classification_targets
@@ -13,7 +12,7 @@ from tqdm import tqdm
 
 class Estimator(object):
 
-    def __init__(self, eps_0=1, p=0.5, N=45):
+    def __init__(self, eps_0=1, p_=0.5, N_=45):
 
         self.eps_0 = eps_0
         self.tol_ = 0
@@ -25,8 +24,8 @@ class Estimator(object):
         self.train_samples_ = 0
         self.coeff_ = np.empty(0)
 
-        self.p_ = p
-        self.N_ = N
+        self.p_ = p_
+        self.N_ = N_
 
         self.X_, self.y_ = np.empty(0), np.empty(0)
         self.X_train_, self.y_train_ = np.empty(0), np.empty(0)
@@ -37,6 +36,101 @@ class Estimator(object):
                f'tol={self.tol_}, ' \
                f'p={self.p_}, ' \
                f'N={self.N_})'
+
+    @classmethod
+    def _get_param_names(cls):
+        """Get parameter names for the estimator"""
+        # fetch the constructor or the original constructor before
+        # deprecation wrapping if any
+        init = getattr(cls.__init__, 'deprecated_original', cls.__init__)
+        if init is object.__init__:
+            # No explicit constructor to introspect
+            return []
+
+        # introspect the constructor arguments to find the model parameters
+        # to represent
+        init_signature = inspect.signature(init)
+        # Consider the constructor parameters excluding 'self'
+        parameters = [p for p in init_signature.parameters.values()
+                      if p.name != 'self' and p.kind != p.VAR_KEYWORD]
+        for p in parameters:
+            if p.kind == p.VAR_POSITIONAL:
+                raise RuntimeError("scikit-learn estimators should always "
+                                   "specify their parameters in the signature"
+                                   " of their __init__ (no varargs)."
+                                   " %s with constructor %s doesn't "
+                                   " follow this convention."
+                                   % (cls, init_signature))
+        # Extract and sort argument names excluding 'self'
+        return sorted([p.name for p in parameters])
+
+    def get_params(self, deep=True):
+        """
+        Get parameters for this estimator.
+
+        Parameters
+        ----------
+        deep : bool, default=True
+            If True, will return the parameters for this estimator and
+            contained subobjects that are estimators.
+
+        Returns
+        -------
+        params : dict
+            Parameter names mapped to their values.
+        """
+        out = dict()
+        for key in self._get_param_names():
+            value = getattr(self, key)
+            if deep and hasattr(value, 'get_params'):
+                deep_items = value.get_params().items()
+                out.update((key + '__' + k, val) for k, val in deep_items)
+            out[key] = value
+        return out
+
+    def set_params(self, **params):
+        """
+        Set the parameters of this estimator.
+
+        The method works on simple estimators as well as on nested objects
+        (such as :class:`~sklearn.pipeline.Pipeline`). The latter have
+        parameters of the form ``<component>__<parameter>`` so that it's
+        possible to update each component of a nested object.
+
+        Parameters
+        ----------
+        **params : dict
+            Estimator parameters.
+
+        Returns
+        -------
+        self : estimator instance
+            Estimator instance.
+        """
+        if not params:
+            # Simple optimization to gain speed (inspect is slow)
+            return self
+        valid_params = self.get_params(deep=True)
+
+        nested_params = defaultdict(dict)  # grouped by prefix
+        for key, value in params.items():
+            key, delim, sub_key = key.partition('__')
+            if key not in valid_params:
+                raise ValueError('Invalid parameter %s for estimator %s. '
+                                 'Check the list of available parameters '
+                                 'with `estimator.get_params().keys()`.' %
+                                 (key, self))
+
+            if delim:
+                nested_params[key][sub_key] = value
+            else:
+                setattr(self, key, value)
+                valid_params[key] = value
+
+        for key, sub_params in nested_params.items():
+            valid_params[key].set_params(**sub_params)
+
+        return self
 
     # --------------------------------------------------------------
 
